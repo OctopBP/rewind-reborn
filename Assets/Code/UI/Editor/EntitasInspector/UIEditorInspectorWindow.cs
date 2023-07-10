f96de9cc3a5bb0d47e526539ee732b403e3a1bea
Boris Proshin <kaktusao@mail.ru>
2023-07-11 02:19:40 +0300

    Move inspector to other folder

--- Assets/Code/UI/Editor/EntitasInspector/UIEditorInspectorWindow.cs
+++ Assets/Code/UI/Editor/EntitasInspector/UIEditorInspectorWindow.cs
@@ -1,9 +1,12 @@
-using System;
+using DesperateDevs.Reflection;
 using Entitas;
+using Entitas.VisualDebugging.Unity.Editor;
+using LanguageExt;
 using Rewind.Extensions;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
+using static LanguageExt.Prelude;
 
 public class EntitasInspector : EditorWindow {
 	// Parent element for dynamically changing UI elements
@@ -12,9 +15,13 @@ public class EntitasInspector : EditorWindow {
 	VisualTreeAsset entityRowVisualTree;
 	VisualTreeAsset componentTagVisualTree;
 
-	const string EntitasInspectorPath = "Assets/Code/Helpers/UI/EntitasInspector/EntitasInspector.uxml";
-	const string EntitasRowPath = "Assets/Code/Helpers/UI/EntitasInspector/EntityRow.uxml";
-	const string ComponentTagPath = "Assets/Code/Helpers/UI/EntitasInspector/ComponentTag.uxml";
+	Option<GameEntity> maybeSelectedGameEntity;
+	Option<int> maybeSelectedComponentIndex;
+
+	const string BasePath = "Assets/Code/UI/Editor/EntitasInspector";
+	const string EntitasInspectorPath = BasePath + "/EntitasInspector.uxml";
+	const string EntitasRowPath = BasePath + "/EntityRow.uxml";
+	const string ComponentTagPath = BasePath + "/ComponentTag.uxml";
 
 	[MenuItem("Tools/EntitasInspector")]
 	public static void showWindow() {
@@ -35,10 +42,49 @@ public class EntitasInspector : EditorWindow {
 		// Load additional UI elements from a different UXML file
 		entityRowVisualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>(EntitasRowPath);
 		componentTagVisualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>(ComponentTagPath);
+		
+		var inspectorContainer = mainUI.Q<VisualElement>("inspector");
+		var imguiContainer = new IMGUIContainer(drawEntityInspector);
+		inspectorContainer.Add(imguiContainer);
 	}
 
 	void Update() {
 		createRows();
+	}
+
+	void drawEntityInspector() {
+		maybeSelectedGameEntity.IfSome(entity => maybeSelectedComponentIndex.Match(
+			componentIndex => drawComponent(entity, entity.GetComponents()[componentIndex], componentIndex),
+			() => EntityDrawer.DrawComponents(entity)
+		));
+	}
+
+	void drawComponent(IEntity entity, IComponent component, int index) {
+		var type = component.GetType();
+		
+		// getTypeDrawer(type).DrawAndGetNewValue(type, "", component)
+		
+		var component1 = entity.CreateComponent(index, type);
+		component.CopyPublicMemberValues(component1);
+		
+		var componentDrawer = getComponentDrawer(type);
+		componentDrawer?.DrawComponent(component1);
+	}
+	
+	static ITypeDrawer getTypeDrawer(System.Type type) {
+		foreach (var typeDrawer in EntityDrawer._typeDrawers) {
+			if (typeDrawer.HandlesType(type))
+				return typeDrawer;
+		}
+		return null;
+	}
+
+	static IComponentDrawer getComponentDrawer(System.Type type) {
+		foreach (var componentDrawer in EntityDrawer._componentDrawers) {
+			if (componentDrawer.HandlesType(type))
+				return componentDrawer;
+		}
+		return null;
 	}
 
 	void createRows() {
@@ -50,6 +96,11 @@ public class EntitasInspector : EditorWindow {
 		rowsContainer.itemsSource = listData;
 		rowsContainer.bindItem = bindItem;
 		rowsContainer.makeItem = makeItem;
+		rowsContainer.selectedIndicesChanged += ints => {
+			var data = Contexts.sharedInstance.game.GetEntities();
+			maybeSelectedGameEntity = ints.HeadOrNone().Map(index => data[index]);
+			maybeSelectedComponentIndex = None;
+		};
 
 		VisualElement makeItem() => entityRowVisualTree.CloneTree();
 
@@ -66,17 +117,22 @@ public class EntitasInspector : EditorWindow {
 	void createRow(VisualElement tagsContainer, IEntity entity) {
 		tagsContainer.Clear();
 
-		foreach (var component in entity.GetComponents()) {
+		var components = entity.GetComponents();
+		for (var index = 0; index < components.Length; index++) {
+			var component = components[index];
 			var componentTag = componentTagVisualTree.CloneTree();
 			var tagButton = componentTag.Q<Button>("tag");
-			
+
+			var idx = index;
+			tagButton.clicked += () => maybeSelectedComponentIndex = idx;
+
 			var componentType = component.GetType();
 			var componentName = componentType.Name.RemoveComponentSuffix();
-			
+
 			var color = ColorExtensions.randomColorForHashCode(componentName.GetHashCode());
 			tagButton.text = componentName;
-			tagButton.style.backgroundColor = color;
-			
+			tagButton.style.backgroundColor = color.withAlpha(.2f);
+
 			tagsContainer.Add(componentTag);
 		}
 	}
